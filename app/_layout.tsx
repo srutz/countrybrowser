dcf2458127852540a4b230144104966557c426d4
stepan.rutz <stepan.rutz@gmx.de>
2024-10-16 22:34:52 +0200

    nativewind

---
+++ app/_layout.tsx
@@ -0,0 +1,15 @@
+import { Stack } from "expo-router";
+
+import { NativeWindStyleSheet } from "nativewind";
+
+NativeWindStyleSheet.setOutput({
+    default: "native",
+});
+
+export default function RootLayout() {
+    return (
+        <Stack>
+            <Stack.Screen name="index" options={{ headerShown: true, headerTitle: "yo" }} />
+        </Stack>
+    )
+}
---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { ScrollView, Text, View } from "react-native";
+
+export default function Page() {
+    return (
+        <View className="bg-1 h-full">
+            <Text className="text-2xl uppercase">Wexxlcome 1111223</Text>
+            <ScrollView>
+                <View className="w-full min-h-[85%] bg-2">
+                    <Text className="text-2xl uppercase">Welcome 1111223</Text>
+                </View>
+            </ScrollView>
+        </View>
+    )
+
+
+}
